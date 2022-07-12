8386b1cf2c69e2e9f259b88764a35db3198adab0
mercy-yugi <mercyyugi@gmail.com>
2022-07-12 11:16:58 +0300

    my files

--- app/src/main/java/com/yugi/workoutlog/SplashActivity.kt
+++ app/src/main/java/com/yugi/workoutlog/SplashActivity.kt
@@ -1,11 +1,13 @@
 package com.yugi.workoutlog
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val intent=Intent(this,loginActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/yugi/workoutlog/loginActivity.kt
+++ app/src/main/java/com/yugi/workoutlog/loginActivity.kt
@@ -2,10 +2,39 @@ package com.yugi.workoutlog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.google.android.material.textfield.TextInputLayout
 
 class loginActivity : AppCompatActivity() {
+    lateinit var btnLogin:Button
+    lateinit var tilEmail:TextInputLayout
+    lateinit var tilPassword:TextInputLayout
+    lateinit var etEmail:EditText
+    lateinit var etPassword:EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btnLogin=findViewById(R.id.btnLogin)
+        tilEmail=findViewById(R.id.tilEmail)
+        tilPassword=findViewById(R.id.tilPassword)
+        etEmail=findViewById(R.id.etEmail)
+        etPassword=findViewById(R.id.etPassword)
+
+        btnLogin.setOnClickListener {
+            validateLogin()
+        }
+    }
+    fun validateLogin(){
+        var email=etEmail.text.toString()
+        var password=etPassword.text.toString()
+
+        if (email.isBlank()){
+            tilEmail.error=getString(R.string.email_required)
+        }
+        if (password.isBlank()){
+            tilPassword.error=getString(R.string.password_required)
+        }
     }
 }
